6d429e254d32086fb81f91332003226fbc6c32ce
yeh-sudo <yehchanshen@gmail.com>
2022-01-06 00:13:30 +0800

    add story

--- Assets/script/storyToMenu.cs
+++ Assets/script/storyToMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class switchToStory : MonoBehaviour{
+public class storyToMenu : MonoBehaviour{
     public void back() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
     }
